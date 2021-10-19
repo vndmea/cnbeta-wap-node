03534a023bb357310ad8705ac394d6fa157a1f02
fanwenzhong <fanwenzhong@ultrapower.com.cn>
2021-10-19 20:39:31 +0800

    增加分页逻辑

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var request = require("request-promise");
 var cheerio = require("cheerio");
 
-router.get("/", function (_, res) {
-  console.log("index route");
-  request("https://m.cnbeta.com/wap")
+// 当路径中传入数字用于分页
+function handler(req, res) {
+  var page = req.params.page;
+  if (page) {
+    if (isNaN(parseInt(page))) {
+      return res.json({ message: "error", data: null, success: false });
+    }
+
+    // 第1页和第0页同时获取
+    page = parseInt(page) > 1 ? parseInt(page) : 0; 
+  }
+
+  request("https://m.cnbeta.com/wap" + (page ? "/index.htm?page=" + page : ""))
     .then(function (htmlString) {
       var $ = cheerio.load(htmlString);
       var data = $("div.list a")
@@ -21,6 +31,12 @@ router.get("/", function (_, res) {
     .catch(function () {
       return res.json({ message: "error", data: null, success: false });
     });
-});
+}
+
+// 全站入口，不带分页参数
+router.get("/", handler);
+
+// 分页参数
+router.get("/:page", handler);
 
 module.exports = router;
